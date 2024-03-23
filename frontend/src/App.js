5137ca2129ba1d28f645ca579626c077677d3b19
Shaun Gan <shaunganyewhan@gmail.com>
2024-03-24 02:01:19 +0800

    Added toggle PW visibility. Added createUserInfo after Sign Up. Bless

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,42 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
-import Login from './pages/login'
-import Signup from './pages/signup'
-import './App.css'
-import { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import CreateUserInfo from "./pages/createUserInfo";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [email, setEmail] = useState('')
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [email, setEmail] = useState("");
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />} />
-          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
-          <Route path="/signup" element={<Signup setLoggedIn={setLoggedIn} setEmail={setEmail} />}/>
+          <Route
+            path="/"
+            element={
+              <Home
+                email={email}
+                loggedIn={loggedIn}
+                setLoggedIn={setLoggedIn}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
+          />
+          <Route
+            path="/signup"
+            element={<Signup setLoggedIn={setLoggedIn} setEmail={setEmail} />}
+          />
+          <Route path="/createUserInfo" element={<CreateUserInfo />} />
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
