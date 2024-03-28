47b81c1d1814e7c3a026ea4b1ea4dbef82d37373
Topheral <sekolah.ib56@gmail.com>
2024-03-28 22:25:05 +0800

    add profile page

--- viteFrontend/src/App.jsx
+++ viteFrontend/src/App.jsx
@@ -11,7 +11,8 @@ import CreateSession from "./pages/CreateSession";
 import Onboarding1 from "./pages/onboardingPg1";
 import Onboarding2 from "./pages/onboardingPg2";
 import Onboarding3 from "./pages/onboardingPg3";
-//import Explore from './pages/explore';
+import Profile from "./pages/Profile";
+
 import "./App.css";
 import { useEffect, useReducer, useState } from "react";
 
@@ -63,6 +64,7 @@ function App() {
           <Route path="/onboardingPg1" element={<Onboarding1 />} />
           <Route path="/onboardingPg2" element={<Onboarding2 />} />
           <Route path="/onboardingPg3" element={<Onboarding3 />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ viteFrontend/src/pages/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+
+
+const Profile = () => {
+  return (
+    <div>
+      <Navbar/>
+      <h1>This is the profile page</h1>
+    </div>
+  );
+};
+
+export default Profile;
+
