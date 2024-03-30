054bb8aa1a9d793f3231469556ad199e4ac11881
Topheral <sekolah.ib56@gmail.com>
2024-03-31 06:43:37 +0800

    edit start page

--- viteFrontend/src/pages/onboardingPg1.jsx
+++ viteFrontend/src/pages/onboardingPg1.jsx
@@ -1,41 +1,3 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import SwoleMates from "../assets/SwoleMates.png";
-// import OB1 from "../assets/OB1.png";
-
-// const Onboarding1 = (props) => {
-
-//     const navigate = useNavigate();
-
-//     const onButtonClick = () => {
-//         navigate("/onboardingPg2");
-//     };
-
-//     return (
-//         <div>          
-//             <div className={"onboardingContainer"}>
-//                 <img src={OB1} alt="Nearby Gym" style={{marginRight:'-40px'}}/>
-//                 Discover <span style={{ color: "red", display: "contents" }}>nearby gyms</span> and how to get there
-//                 </div>
-//             <br />
-            
-//             <div className={"inputContainer"}>
-//                 <input
-//                 className={"inputButton"}
-//                 type="button"
-//                 onClick={onButtonClick}
-//                 value={"Next"}
-//                 />
-//             </div>
-//         </div>
-//     );
-
-// }
-
-// export default Onboarding1;
-
-
-
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import OB1 from "../assets/OB1.png";
@@ -97,3 +59,40 @@ const Onboarding1 = (props) => {
 }
 
 export default Onboarding1;
+
+
+// import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+// import SwoleMates from "../assets/SwoleMates.png";
+// import OB1 from "../assets/OB1.png";
+
+// const Onboarding1 = (props) => {
+
+//     const navigate = useNavigate();
+
+//     const onButtonClick = () => {
+//         navigate("/onboardingPg2");
+//     };
+
+//     return (
+//         <div>          
+//             <div className={"onboardingContainer"}>
+//                 <img src={OB1} alt="Nearby Gym" style={{marginRight:'-40px'}}/>
+//                 Discover <span style={{ color: "red", display: "contents" }}>nearby gyms</span> and how to get there
+//                 </div>
+//             <br />
+            
+//             <div className={"inputContainer"}>
+//                 <input
+//                 className={"inputButton"}
+//                 type="button"
+//                 onClick={onButtonClick}
+//                 value={"Next"}
+//                 />
+//             </div>
+//         </div>
+//     );
+
+// }
+
+// export default Onboarding1;
