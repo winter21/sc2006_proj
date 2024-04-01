2c5b47536262d9681fb2710b515644759bed5bf5
lk-lk <111081118+lesterkzw@users.noreply.github.com>
2024-04-01 22:02:37 +0800

    added joinWorkoutSession, LeaveWorkoutsession, getUserWorkoutsession, getHostWorkoutSession

--- SwoleMates Server/src/routes/workoutSessionRouter.js
+++ SwoleMates Server/src/routes/workoutSessionRouter.js
@@ -17,4 +17,16 @@ router.put("/:id",workoutSessionController.updateWorkoutSession);
 //DELETE method to delete one workout session
 router.delete("/:id",workoutSessionController.deleteWorkoutSession);
 
+//joinWorkout session
+router.post("/join/:id", workoutSessionController.joinWorkoutSession);
+
+//leaveWorkout session
+router.post("/leave/:id", workoutSessionController.leaveWorkoutSession);
+
+//getUserWorkoutSessions
+router.get("/user/:id", workoutSessionController.getUserWorkoutSessions);
+
+//getHostWorkoutSessions
+router.get("/host/:userId", workoutSessionController.getHostWorkoutSessions);
+
 module.exports = router;
