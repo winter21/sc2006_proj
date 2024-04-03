33b007dfef476da81b73570da22422102af51836
Topheral <sekolah.ib56@gmail.com>
2024-04-03 21:21:41 +0800

    edit home page

--- viteFrontend/src/pages/home.jsx
+++ viteFrontend/src/pages/home.jsx
@@ -4,7 +4,7 @@ import SwoleMates from "../assets/SwoleMates.png";
 import UluPandan from "../assets/UluPandan.png";
 import Navbar from "../components/Navbar";
 import axios from "axios";
-import { TextField, Box, Typography, CardContent, Card, Grid } from "@mui/material"; // Import MUI components
+import { TextField, Box, Typography, CardContent, Card, Grid, List, ListItem } from "@mui/material"; // Import MUI components
 import SearchIcon from "@mui/icons-material/Search";
 
 const Home = (props) => {
@@ -31,6 +31,21 @@ const Home = (props) => {
 
   const handleInputBlur = () => {
     setIsInputFocused(false);
+  };
+
+  const scrollContainerStyle = {
+    display: 'flex',
+    overflowX: 'auto',
+    '&::-webkit-scrollbar': {
+      height: '8px', // You can adjust the height of the scrollbar here
+    },
+    '&::-webkit-scrollbar-thumb': {
+      background: '#888',
+      borderRadius: '4px',
+    },
+    '&::-webkit-scrollbar-thumb:hover': {
+      background: '#555',
+    },
   };
 
   const filteredSessions = sessions.filter(session =>
@@ -74,6 +89,25 @@ const Home = (props) => {
           </Grid>
         ))}
       </Grid>
+
+      <Box sx={scrollContainerStyle}>
+        <List style={{ display: 'flex' }}>
+          {filteredSessions.map((session) => (
+            <ListItem key={session.id} style={{ display: 'inline-block', width: 'auto', marginRight: '20px'}}>
+              <Card elevation={6} className="customCard" sx={{ minWidth: 275, maxWidth: 345, borderRadius: '20px', boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.5)' }}>
+                <CardContent>
+                  <img src={UluPandan} alt="Ulu Pandan" style={{ width: '100%', height: 'auto', marginBottom: '10px', borderRadius: '8%' }} />
+                  <Typography variant="h5" component="h2" style={{ fontWeight: 'bold' }}>{session.name}</Typography>
+                  <Typography variant="body1">Location: {session.address}</Typography>
+                  <Typography variant="body1">Date: {session.date}</Typography>
+                  <Typography variant="body1">Slots: {session.slots} slot(s) left</Typography>
+                </CardContent>
+              </Card>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+
     </div>
   );
 };
