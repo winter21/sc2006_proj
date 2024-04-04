bd4fcc7ec738f55b68195fbfc9a7800d059b4e4b
KX <tkaixuan2014@gmail.com>
2024-04-05 04:36:37 +0800

    create user info done, 1 bug found, please test

--- viteFrontend/src/pages/CreateSession.jsx
+++ viteFrontend/src/pages/CreateSession.jsx
@@ -40,9 +40,9 @@ const CreateSession = () => {
   };
 
   const handleSessionPictureChange = (file) => {
-    setSessionPicture(file); 
+    setSessionPicture(file);
   };
-  
+
   const handleAddress = (location) => {
     setAddress(location);
   };
@@ -52,7 +52,7 @@ const CreateSession = () => {
     googleMapsApiKey: "AIzaSyDKEBSYBdvZtuTcN7Lx8Mg6RTBaGtPCOQY",
     libraries: libraries,
   });
-  const searchInput = useRef()
+  const searchInput = useRef();
   //const user = JSON.parse(localStorage.getItem("userID"))
 
   function onLoad(autocomplete) {
@@ -73,30 +73,30 @@ const CreateSession = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     handleAddress(searchInput.current.value);
-    setDurationError("")
-    setSlotsError("")
-    setNameError("")
-    setLocationError("")
-    setDateError("")
-    setTimeError("")
+    setDurationError("");
+    setSlotsError("");
+    setNameError("");
+    setLocationError("");
+    setDateError("");
+    setTimeError("");
 
     if ("" === name) {
       setNameError("Please enter a session name");
     }
 
-    if ("" === duration) { 
+    if ("" === duration) {
       setDurationError("Please enter a duration");
     } else if (!isInteger(duration)) {
       setDurationError("Please enter a valid integer for duration");
     }
-    
-    if ("" === slots) { 
+
+    if ("" === slots) {
       setSlotsError("Please enter the number of slots");
     } else if (!isInteger(slots)) {
       setSlotsError("Please enter a valid integer for the number of slots");
     }
-    
-    if (searchInput.current.value ===  "") {
+
+    if (searchInput.current.value === "") {
       setLocationError("Please select a location");
     }
 
@@ -104,40 +104,51 @@ const CreateSession = () => {
       setDateError("Please select a session date");
     }
 
-    if ("" === startHr || "" === startMin ) { 
+    if ("" === startHr || "" === startMin) {
       setTimeError("Please enter a start time");
-    } else if (!isInteger(startHr) || !isInteger(startMin) ) {
+    } else if (!isInteger(startHr) || !isInteger(startMin)) {
       setTimeError("Please enter a valid integer for the start time");
-    } else if ((startHr>23) || (startMin>59)) {
+    } else if (startHr > 23 || startMin > 59) {
       setTimeError("Please enter a valid time");
-    } else if ((startHr.length !== 2) || (startMin.length !== 2)) {
+    } else if (startHr.length !== 2 || startMin.length !== 2) {
       setTimeError("Please enter a start time in 24hr format");
     }
 
-    if (!dateError === "" && !locationError === "" && !timeError === "" && !durationError === "" && !slotsError === "" && !nameError === "") return;
+    if (
+      !dateError === "" &&
+      !locationError === "" &&
+      !timeError === "" &&
+      !durationError === "" &&
+      !slotsError === "" &&
+      !nameError === ""
+    )
+      return;
     createSes();
-  }
+  };
 
   const createSes = async () => {
     try {
       const formData = new FormData();
-      const token = JSON.parse(localStorage.getItem("user")).token
-      const decodedToken = await axios.post("http://localhost:3000/account/decode-jwt", {
-        token: token
-      });
-      const userId = decodedToken.data.userId
-      const date = formatDate(selectedDate)
-      if(!sessionPicture){
-        let blob = await fetch(DefaultAvatar).then(r => r.blob());
+      const token = JSON.parse(localStorage.getItem("user")).token;
+      const decodedToken = await axios.post(
+        "http://localhost:3000/account/decode-jwt",
+        {
+          token: token,
+        }
+      );
+      const userId = decodedToken.data.userId;
+      const date = formatDate(selectedDate);
+      if (!sessionPicture) {
+        let blob = await fetch(DefaultAvatar).then((r) => r.blob());
         const defaultFile = new File([blob], "DefaultAvatar.png", {
           type: "image/png",
         });
         formData.append("photo", defaultFile);
-      }else{
+      } else {
         formData.append("photo", sessionPicture);
       }
       formData.append("name", name);
-      formData.append("date", ( date + "T" + startHr + ":" + startMin + ":00Z"));
+      formData.append("date", date + "T" + startHr + ":" + startMin + ":00Z");
       formData.append("address", address);
       formData.append("duration", duration);
       formData.append("slots", slots);
@@ -145,27 +156,30 @@ const CreateSession = () => {
       formData.append("interest", interests);
       formData.append("type", "workoutPicture");
       formData.append("on", true);
-      const res = await axios.post("http://localhost:3000/workoutSession/create", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      }); 
+      const res = await axios.post(
+        "http://localhost:3000/workoutSession/create",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
       console.log(res);
       navigate("/home", { replace: true });
     } catch (error) {
-      console.log(error.stack)
+      console.log(error.stack);
     }
   };
-  
+
   function isInteger(value) {
     // Use parseInt to parse the value and check if it's a valid integer
-    return typeof value === 'string' && /^\d+$/.test(value);
+    return typeof value === "string" && /^\d+$/.test(value);
   }
 
   const formatDate = (date) => {
     return format(date, "yyyy-MM-dd");
   };
-
 
   const handleInterestChange = (interest) => {
     if (!interests.includes(interest)) {
@@ -207,157 +221,158 @@ const CreateSession = () => {
     width: "100vw",
     height: "100vh",
     backgroundImage: `url(${BackgroundImage})`,
-    backgroundPosition: 'center',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    position: 'fixed',
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    position: "fixed",
     top: 0,
     left: 0,
-    zIndex: -1
+    zIndex: -1,
   };
 
   const imageContainerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
   };
 
   return (
     <div>
       <Navbar />
-      <div style={fullScreenBackgroundStyle} /> 
+      <div style={fullScreenBackgroundStyle} />
       <div style={containerStyle}>
-      <div className={"mainContainer"}>
-      <div className={"headerContainer"}>
-        <h2>Create a New Workout Session</h2>
+        <div className={"mainContainer"}>
+          <div className={"headerContainer"}>
+            <h2>Create a New Workout Session</h2>
           </div>
-        <form onSubmit={handleSubmit}>
-      <div style={imageContainerStyle}>
-        <input
-          accept="image/*"
-          //className={classes.input}
-          id="contained-button-file"
-          multiple
-          type="file"
-          onChange={(e) =>
-            handleSessionPictureChange(e.target.files[0])
-          }
-          style={{
-            display: "none",
-          }}
-          ref={(input) => {
-            // Store a reference to the input element
-            if (input) {
-              input.value = ""; // Reset the input value
-              input.defaultValue = DefaultAvatar; // Set the default file
-            }
-          }}
-        />
-        <label htmlFor="contained-button-file">
-          <IconButton component="span">
-            <Avatar
-              src={
-                sessionPicture
-                  ? URL.createObjectURL(sessionPicture)
-                  : DefaultAvatar
-              }
-              style={{
-                margin: "10px",
-                width: "150px",
-                height: "150px",
-                borderRadius: "50%",
-                border: "8px solid #ccc",
-              }}
-            />
-          </IconButton>
-        </label></div>
-      <p>Click on the Session Picture to upload an image</p><br/>
-          <div className={"titleContainer"}>
-            <label>Session Name:</label>
-          </div>
-          <div className={"detailsInputContainer"}>
-            <input
-              value={name}
-              placeholder="Zenitsu's Workout Session"
-              onChange={(e) => setName(e.target.value)}
-              className={"inputBox"}
-            />
-            <label className="errorLabel">{nameError}</label>
-          </div>
-          <br />
-          <div className={"titleContainer"}>
-            <label>Session Date:</label>
-          </div>
-          <div className={"detailsInputContainer"}>
-            <DatePicker selected={selectedDate} 
-            onChange={handleSelectedDate}
-            dateFormat="yyyy-MM-dd"
-            placeholderText="yyyy-mm-dd"
-            className={"inputBox"}
-            />
-            <label className="errorLabel">{dateError}</label>
-          </div>
-          <br />
-          <div className={"titleContainer"}>
-            <label>Session Start Time (24hr):</label>
-          </div>
-          <div className={"detailsInputContainer"}>
-          <div style={{ display: 'flex'}}>
-          <input
-              value={startHr}
-              placeholder="01"
-              onChange={(e) => setStartHr(e.target.value)}
-              className={"timeInputBox"}
-              maxLength={2} 
-              style={{ marginRight: '10px' }} // Adjust the spacing between input boxes
-            />
-            <h3>:</h3> 
-            <input
-            value={startMin}
-            placeholder="23"
-            onChange={(e) => setStartMin(e.target.value)}
-            className={"timeInputBox"}
-            maxLength={2} 
-            style={{ marginLeft: '10px' }} 
-            />
-          </div>
-            <label className="errorLabel">{timeError}</label>
-            </div>
-        
-          <br />
-          <div className={"titleContainer"}>
-            <label>Session Duration (hrs):</label>
-          </div>
-          <div className={"detailsInputContainer"}>
-            <input
-              value={duration}
-              placeholder="3"
-              onChange={(e) => setDuration(e.target.value)}
-              className={"inputBox"}
-            />
-            <label className="errorLabel">{durationError}</label>
-          </div>
-          <br />
-          <div className={"titleContainer"}>
-            <label htmlFor="location-input">Session Location:</label>
-          </div>
-          <div className={"detailsInputContainer"}>
-            <Autocomplete
-              onPlaceChanged={(place) => onPlaceChanged(place)}
-              onLoad={onLoad}
-            >
+          <form onSubmit={handleSubmit}>
+            <div style={imageContainerStyle}>
               <input
-                type="text"
-                placeholder="North Hill Gym"
-                size="auto"
-                ref={searchInput}
+                accept="image/*"
+                //className={classes.input}
+                id="contained-button-file"
+                multiple
+                type="file"
+                onChange={(e) => handleSessionPictureChange(e.target.files[0])}
+                style={{
+                  display: "none",
+                }}
+                ref={(input) => {
+                  // Store a reference to the input element
+                  if (input) {
+                    input.value = ""; // Reset the input value
+                    input.defaultValue = DefaultAvatar; // Set the default file
+                  }
+                }}
+              />
+              <label htmlFor="contained-button-file">
+                <IconButton component="span">
+                  <Avatar
+                    src={
+                      sessionPicture
+                        ? URL.createObjectURL(sessionPicture)
+                        : DefaultAvatar
+                    }
+                    style={{
+                      margin: "10px",
+                      width: "150px",
+                      height: "150px",
+                      borderRadius: "50%",
+                      border: "8px solid #ccc",
+                    }}
+                  />
+                </IconButton>
+              </label>
+            </div>
+            <p>Click on the Session Picture to upload an image</p>
+            <br />
+            <div className={"titleContainer"}>
+              <label>Session Name:</label>
+            </div>
+            <div className={"detailsInputContainer"}>
+              <input
+                value={name}
+                placeholder="Zenitsu's Workout Session"
+                onChange={(e) => setName(e.target.value)}
                 className={"inputBox"}
               />
-            </Autocomplete>
+              <label className="errorLabel">{nameError}</label>
+            </div>
+            <br />
+            <div className={"titleContainer"}>
+              <label>Session Date:</label>
+            </div>
+            <div className={"detailsInputContainer"}>
+              <DatePicker
+                selected={selectedDate}
+                onChange={handleSelectedDate}
+                dateFormat="yyyy-MM-dd"
+                placeholderText="yyyy-mm-dd"
+                className={"inputBox"}
+              />
+              <label className="errorLabel">{dateError}</label>
+            </div>
+            <br />
+            <div className={"titleContainer"}>
+              <label>Session Start Time (24hr):</label>
+            </div>
+            <div className={"detailsInputContainer"}>
+              <div style={{ display: "flex" }}>
+                <input
+                  value={startHr}
+                  placeholder="01"
+                  onChange={(e) => setStartHr(e.target.value)}
+                  className={"timeInputBox"}
+                  maxLength={2}
+                  style={{ marginRight: "10px" }} // Adjust the spacing between input boxes
+                />
+                <h3>:</h3>
+                <input
+                  value={startMin}
+                  placeholder="23"
+                  onChange={(e) => setStartMin(e.target.value)}
+                  className={"timeInputBox"}
+                  maxLength={2}
+                  style={{ marginLeft: "10px" }}
+                />
+              </div>
+              <label className="errorLabel">{timeError}</label>
+            </div>
+
+            <br />
+            <div className={"titleContainer"}>
+              <label>Session Duration (hrs):</label>
+            </div>
+            <div className={"detailsInputContainer"}>
+              <input
+                value={duration}
+                placeholder="3"
+                onChange={(e) => setDuration(e.target.value)}
+                className={"inputBox"}
+              />
+              <label className="errorLabel">{durationError}</label>
+            </div>
+            <br />
+            <div className={"titleContainer"}>
+              <label htmlFor="location-input">Session Location:</label>
+            </div>
+            <div className={"detailsInputContainer"}>
+              <Autocomplete
+                onPlaceChanged={(place) => onPlaceChanged(place)}
+                onLoad={onLoad}
+              >
+                <input
+                  type="text"
+                  placeholder="North Hill Gym"
+                  size="auto"
+                  ref={searchInput}
+                  className={"inputBox"}
+                />
+              </Autocomplete>
               <label className="errorLabel">{locationError}</label>
-          </div>
-          {/* <label>Session coordinates:</label>
+            </div>
+            {/* <label>Session coordinates:</label>
         <input
           value={coordinates.longitude} 
           placeholder="Longitude, eg: 90"
@@ -375,47 +390,47 @@ const CreateSession = () => {
             latitude: e.target.value
           }))}className={"inputBox"}
           /></div><br/> */}
-          <br />
-          <div className={"titleContainer"}>
-            <label>Max Number of Participants:</label>
-          </div>
-          <div className={"detailsInputContainer"}>
-            <input
-              value={slots}
-              placeholder="30"
-              onChange={(e) => setSlots(e.target.value)}
-              className={"inputBox"}
-            />
-            <label className="errorLabel">{slotsError}</label>
-          </div>
-          <br />
-          <div className={"titleContainer"}>
-            <label>Type of Session:</label>
-          </div>
-          <div className={"interestsContainer"} >
-            {workoutInterestExamples.map((interest, index) => (
-              <div
-                key={index}
-                className={`interestItem ${
-                  interests.includes(interest) ? "selectedWorkout" : ""
-                }`}
-                onClick={() => handleInterestChange(interest)}
-              >
-                {interest}
-              </div>
-            ))}
-          </div>
-          <br />
-          <div className={"inputContainer"}>
-            <input
-              className={"inputButton"}
-              type="submit"
-              value={"Add Session"}
-            />
-          </div>
-        </form>
+            <br />
+            <div className={"titleContainer"}>
+              <label>Max Number of Participants:</label>
+            </div>
+            <div className={"detailsInputContainer"}>
+              <input
+                value={slots}
+                placeholder="30"
+                onChange={(e) => setSlots(e.target.value)}
+                className={"inputBox"}
+              />
+              <label className="errorLabel">{slotsError}</label>
+            </div>
+            <br />
+            <div className={"titleContainer"}>
+              <label>Type of Session:</label>
+            </div>
+            <div className={"interestsContainer"}>
+              {workoutInterestExamples.map((interest, index) => (
+                <div
+                  key={index}
+                  className={`interestItem ${
+                    interests.includes(interest) ? "selectedWorkout" : ""
+                  }`}
+                  onClick={() => handleInterestChange(interest)}
+                >
+                  {interest}
+                </div>
+              ))}
+            </div>
+            <br />
+            <div className={"inputContainer"}>
+              <input
+                className={"inputButton"}
+                type="submit"
+                value={"Add Session"}
+              />
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
