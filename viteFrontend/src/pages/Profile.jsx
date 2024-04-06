e6e3bcd6b757fe6934dc0e4e084053bd831f3713
KX <tkaixuan2014@gmail.com>
2024-04-06 15:57:28 +0800

    profile page fetching info

--- viteFrontend/src/pages/Profile.jsx
+++ viteFrontend/src/pages/Profile.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import DefaultAvatar from "../assets/Zenitsu.png";
 import "../profile.css";
 
 const Profile = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [userData, setUserData] = useState({});
   const navigate = useNavigate();
   const handleSignOut = () => {
     //  the sign-out logic (like clearing tokens)
@@ -14,59 +16,102 @@ const Profile = (props) => {
     props.setUsername("");
     navigate("/login");
   };
-  console.log("testing");
-
   console.log("run");
-  const verify = async () => {
-    try {
+  function indexToColor(index) {
+    const colors = [
+      "#FF7043", // Deep orange
+      "#AB47BC", // Purple
+      "#5C6BC0", // Indigo
+      "#42A5F5", // Blue
+      "#26A69A", // Teal
+      "#9CCC65", // Light green
+      "#D4E157", // Lime
+      "#EF5350", // Red
+      "#EC407A", // Pink
+      "#FFA726", // Orange
+      "#8D6E63", // Brown
+      "#78909C", // Blue Grey
+    ];
+    return colors[index % colors.length]; // Use modulo to ensure cycling through colors
+  }
+  const genderLabels = {
+    MALE: "Male",
+    FEMALE: "Female",
+    "PREFER NOT TO SAY": "Other",
+  };
+  useEffect(() => {
+    const verify = async () => {
       const user = JSON.parse(localStorage.getItem("user"));
-      const res = await axios.post("http://localhost:3000/account/check-jwt", {
-        token: user.token,
-      });
-      console.log(res);
-      console.log(res.data.userId);
-    } catch (error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        console.error("Server responded with status:", error.response.status);
-        console.error("Response data:", error.response.data);
-        console.error("Response headers:", error.response.headers);
-        window.alert(error.response.data.message);
-      } else if (error.request) {
-        // The request was made but no response was received
-        console.error("No response received:", error);
-        window.alert("No response received from server: " + error.message);
-      } else {
-        // Something happened in setting up the request that triggered an error
-        console.error("Error setting up the request:", error.message);
-        window.alert("Error setting up the request: " + error.message);
+      try {
+        const res = await axios.post(
+          "http://localhost:3000/account/check-jwt",
+          {
+            token: user.token,
+          }
+        );
+        console.log(res);
+        console.log(res.data.userId);
+        const resp = await axios.get(
+          `http://localhost:3000/user/${res.data.userId}`
+        );
+        console.log("userData:");
+        console.log(resp.data);
+        setUserData({
+          name: resp.data.name,
+          email: resp.data.email,
+          gender: genderLabels[resp.data.gender],
+          aboutMe: resp.data.aboutMe,
+          interests: resp.data.interest.map((interest, index) => ({
+            name: interest,
+            color: indexToColor(index), // Generate color dynamically
+          })),
+          profilePicUrl: DefaultAvatar, // Placeholder image URL
+        });
+      } catch (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          console.error("Server responded with status:", error.response.status);
+          console.error("Response data:", error.response.data);
+          console.error("Response headers:", error.response.headers);
+          window.alert(error.response.data.message);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.error("No response received:", error);
+          window.alert("No response received from server: " + error.message);
+        } else {
+          // Something happened in setting up the request that triggered an error
+          console.error("Error setting up the request:", error.message);
+          window.alert("Error setting up the request: " + error.message);
+        }
       }
-    }
-  };
-  verify();
-  // Mock user data
-  const userData = {
-    name: "Zenitsu Agatsuma",
-    email: "Zenitsu.Agatsuma@gmail.com",
-    gender: "Male",
-    aboutMe:
-      "Hey there, I'm Zenitsu Agatsuma, possibly the most reluctant Demon Slayer you'll ever meet. Despite my tendency to, well, freak out at the slightest hint of danger, I've somehow landed myself in the thick of demon-slaying action. Trained in the art of Thunder Breathing—which, trust me, is way cooler in action than it sounds—I've got this unique knack of becoming a formidable fighter the moment I pass out from fear. It's a weird talent, but it's saved my skin more times than I can count. Off the battlefield, you might find me swooning over any girl who gives me the time of day or indulging in my sweet tooth (I have a bit of a thing for anything sugary). Despite my perpetual state of panic and my, uh, less-than-stellar confidence, I dream of a peaceful life free from demons. But until that day comes, I'll be here, zapping demons in my sleep and trying to be the bravest version of myself, one terrified step at a time.",
-    interests: [
-      { name: "Running", color: "#FF7043" }, // Deep orange
-      { name: "HIIT", color: "#AB47BC" }, // Purple
-      { name: "Yoga", color: "#5C6BC0" }, // Indigo
-      { name: "Pilates", color: "#42A5F5" }, // Blue
-      { name: "Dance", color: "#26A69A" }, // Teal
-      { name: "Hiking", color: "#9CCC65" }, // Light green
-      { name: "Boxing", color: "#D4E157" }, // Lime
-      { name: "Cycling", color: "#EF5350" }, // Red
-      { name: "Rowing", color: "#EC407A" }, // Pink
-      { name: "Weightlifting", color: "#FFA726" }, // Orange
-      { name: "Swimming", color: "#8D6E63" }, // Brown
-      { name: "CrossFit", color: "#78909C" }, // Blue Grey
-    ],
-    profilePicUrl: DefaultAvatar, // Placeholder image URL
-  };
+    };
+    verify();
+  }, []);
+  /*useEffect(() => {
+    // Mock user data
+    setUserData({
+      name: "Zenitsu Agatsuma",
+      email: "Zenitsu.Agatsuma@gmail.com",
+      gender: "Male",
+      aboutMe:
+        "Hey there, I'm Zenitsu Agatsuma, possibly the most reluctant Demon Slayer you'll ever meet. Despite my tendency to, well, freak out at the slightest hint of danger, I've somehow landed myself in the thick of demon-slaying action. Trained in the art of Thunder Breathing—which, trust me, is way cooler in action than it sounds—I've got this unique knack of becoming a formidable fighter the moment I pass out from fear. It's a weird talent, but it's saved my skin more times than I can count. Off the battlefield, you might find me swooning over any girl who gives me the time of day or indulging in my sweet tooth (I have a bit of a thing for anything sugary). Despite my perpetual state of panic and my, uh, less-than-stellar confidence, I dream of a peaceful life free from demons. But until that day comes, I'll be here, zapping demons in my sleep and trying to be the bravest version of myself, one terrified step at a time.",
+      interests: [
+        { name: "Running", color: "#FF7043" }, // Deep orange
+        { name: "HIIT", color: "#AB47BC" }, // Purple
+        { name: "Yoga", color: "#5C6BC0" }, // Indigo
+        { name: "Pilates", color: "#42A5F5" }, // Blue
+        { name: "Dance", color: "#26A69A" }, // Teal
+        { name: "Hiking", color: "#9CCC65" }, // Light green
+        { name: "Boxing", color: "#D4E157" }, // Lime
+        { name: "Cycling", color: "#EF5350" }, // Red
+        { name: "Rowing", color: "#EC407A" }, // Pink
+        { name: "Weightlifting", color: "#FFA726" }, // Orange
+        { name: "Swimming", color: "#8D6E63" }, // Brown
+        { name: "CrossFit", color: "#78909C" }, // Blue Grey
+      ],
+      profilePicUrl: DefaultAvatar, // Placeholder image URL
+    });
+  }, []);*/
   return (
     <div>
       <Navbar />
@@ -79,21 +124,24 @@ const Profile = (props) => {
         <p>Email: {userData.email}</p>
         <p>Gender: {userData.gender}</p>
         <div className="interests-container">
-          {userData.interests.map((interest, index) => (
-            <span
-              key={index}
-              className="interest-tag"
-              style={{ backgroundColor: interest.color }}
-            >
-              {interest.name}
-            </span>
-          ))}
+          {userData.interests &&
+            userData.interests.map((interest, index) => (
+              <span
+                key={index}
+                className="interest-tag"
+                style={{ backgroundColor: interest.color }}
+              >
+                {interest.name}
+              </span>
+            ))}
         </div>
         <div className="about-me">
           <p>About Me:</p>
           <p>{userData.aboutMe}</p>
         </div>
-        <button onClick={handleSignOut}>Sign Out</button>
+        <button id="profSignOut" onClick={handleSignOut}>
+          Sign Out
+        </button>
       </div>
     </div>
   );
