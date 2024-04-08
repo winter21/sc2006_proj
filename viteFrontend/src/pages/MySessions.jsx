4252983850ac167c2e6887cdb62155879e8722a5
Topheral <sekolah.ib56@gmail.com>
2024-04-09 01:24:35 +0800

    mysessions page, added hover effect

--- viteFrontend/src/pages/MySessions.jsx
+++ viteFrontend/src/pages/MySessions.jsx
@@ -71,6 +71,10 @@ const MySessions = () => {
                   maxWidth: 345,
                   borderRadius: "20px",
                   boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.5)",
+                  transition: "transform 0.3s",
+                  "&:hover": {
+                    transform: "scale(1.05)", // This line enables the card to grow when hovered
+                  },
                 }}
               >
                 <CardContent>
@@ -132,58 +136,60 @@ const MySessions = () => {
 export default MySessions;
 
 
-// import React, { useEffect, useState } from 'react';
-// import { useNavigate } from "react-router-dom"; 
-// import axios from 'axios';
-// import Navbar from '../components/Navbar';
+// // the one with toggle button
+// import React, { useState, useEffect } from "react";
+// import Navbar from "../components/Navbar";
+// import { useNavigate } from "react-router-dom";
+// import axios from "axios";
 // import {
+//   TextField,
+//   Box,
 //   Typography,
 //   CardContent,
 //   Card,
+//   Grid,
 //   List,
 //   ListItem,
 // } from "@mui/material";
+// import UluPandan from "../assets/Zenitsu.png";
 
 // const MySessions = () => {
-//   const [hostedSessions, setHostedSessions] = useState([]);
-//   const [joinedSessions, setJoinedSessions] = useState([]);
-//   const [isLoading, setIsLoading] = useState(true);
-//   const navigate = useNavigate();
+//   const [isHosted, setIsHosted] = useState(true);
+
+//   const toggleSessions = () => setIsHosted(!isHosted);
+//   const [sessions, setSessions] = useState([]);
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const [isInputFocused, setIsInputFocused] = useState(false);
 
 //   useEffect(() => {
 //     const fetchSessions = async () => {
-//       const user = JSON.parse(localStorage.getItem("user"));
-//       if (!user || !user.token) {
-//         console.error("No user token found");
-//         setIsLoading(false);
-//         return;
-//       }
-
 //       try {
-//         const verifyRes = await axios.post("http://localhost:3000/account/check-jwt", {
-//           token: user.token,
-//         });
-//         console.log("User ID:", verifyRes.data.userId); // Debugging line
-
-//         // Fetching hosted sessions
-//         const hostedSessionsRes = await axios.get(`http://localhost:3000/workoutSession/host/${verifyRes.data.userId}`);
-//         console.log("Hosted Sessions:", hostedSessionsRes.data); // Debugging line
-//         setHostedSessions(hostedSessionsRes.data);
-
-//         // Fetching joined sessions
-//         const joinedSessionsRes = await axios.get(`http://localhost:3000/workoutSession/user/${verifyRes.data.userId}`);
-//         console.log("Joined Sessions:", joinedSessionsRes.data); // Debugging line
-//         setJoinedSessions(joinedSessionsRes.data);
+//         const res = await axios.post(
+//           "http://localhost:3000/account/check-jwt",
+//           {
+//             token: user.token,
+//           }
+//         );
+//         console.log(res);
+//         console.log(res.data.userId);
+//         const resp = await axios.get(
+//           `http://localhost:3000/user/${res.data.userId}`
+//         );
 //       } catch (error) {
-//         console.error("Failed to fetch sessions:", error);
-//       } finally {
-//         setIsLoading(false);
+//         console.error("Error fetching sessions:", error);
 //       }
 //     };
-
 //     fetchSessions();
 //   }, []);
-  
+
+//   const handleInputFocus = () => {
+//     setIsInputFocused(true);
+//   };
+
+//   const handleInputBlur = () => {
+//     setIsInputFocused(false);
+//   };
+
 //   const scrollContainerStyle = {
 //     display: "flex",
 //     overflowX: "auto",
@@ -194,118 +200,159 @@ export default MySessions;
 //     },
 //     "-ms-overflow-style": "none", // For Internet Explorer and Edge
 //   };
-  
 
-//   const renderSessionsList = (sessions) => (
-//     <>
-//       {sessions.length > 0 ? (
-//         <List style={{ display: 'flex', flexDirection: 'row', padding: '20px', overflowX: 'scroll' }}>
-//           {sessions.map(session => (
-//             <ListItem
-//               key={session._id}
+//   const filteredSessions = sessions.filter((session) =>
+//     session.name.toLowerCase().includes(searchTerm.toLowerCase())
+//   );
+
+//   return (
+//     <div>
+//       <Navbar />
+//       <div
+//         style={{
+//           display: "flex",
+//           justifyContent: "center",
+//           marginTop: "40px",
+//           marginBottom: "30px",
+//         }}
+//       >
+//         <div
+//           onClick={toggleSessions}
+//           style={{
+//             display: "flex",
+//             justifyContent: isHosted ? "flex-start" : "flex-end",
+//             alignItems: "center",
+//             width: "200px",
+//             height: "40px",
+//             padding: "2px",
+//             backgroundColor: "#f0f0f0",
+//             border: "2px solid #dcdcdc",
+//             borderRadius: "20px",
+//             cursor: "pointer",
+//             position: "relative",
+//           }}
+//         >
+//           {/* New Text Element for "Hosted" */}
+//           {isHosted && (
+//             <div
 //               style={{
-//                 display: "inline-block",
-//                 width: "auto",
-//                 marginRight: "20px",
+//                 position: "absolute",
+//                 left: "63%", // Position to the right of the smaller oval
+//                 top: "50%",
+//                 transform: "translateY(-50%)",
+//                 color: "#000",
 //               }}
-//               onClick={() => handleCardClick(session._id)}
 //             >
-//               <Card
-//                 elevation={6}
-//                 sx={{
-//                   minWidth: 275,
-//                   maxWidth: 345,
-//                   borderRadius: "20px",
-//                   boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.5)",
-//                 }}
-//               >
-//                 <CardContent>
-//                   <img
-//                     src={`http://localhost:3000/${session.workoutPicture}`}
-//                     alt={session.name}
-//                     style={{
-//                       width: "100%",
-//                       height: "auto",
-//                       marginBottom: "10px",
-//                       borderRadius: "8%",
+//               Joined
+//             </div>
+//           )}
+
+//           {/* Toggle Indicator */}
+//           <div
+//             style={{
+//               width: "50%",
+//               height: "100%",
+//               backgroundColor: isHosted ? "#FF0000" : "#FF0000",
+//               borderRadius: "20px",
+//               textAlign: "center",
+//               lineHeight: "36px",
+//               color: "white",
+//             }}
+//           >
+//             {isHosted ? "Hosted" : "Joined"}
+//           </div>
+
+//           {/* New Text Element for "Joined" */}
+//           {!isHosted && (
+//             <div
+//               style={{
+//                 position: "absolute",
+//                 right: "63%", // Position to the left of the smaller oval
+//                 top: "50%",
+//                 transform: "translateY(-50%)",
+//                 color: "#000",
+//               }}
+//             >
+//               Hosted
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//       {isHosted ? (
+//         <div>
+//           <p>
+//             Showing{" "}
+//             <span style={{ fontSize: "120px", fontWeight: "bold" }}>
+//               Hosted
+//             </span>{" "}
+//             Sessions...
+//           </p>
+//           <Box sx={scrollContainerStyle}>
+//             <List style={{ display: "flex" }}>
+//               {filteredSessions.map((session) => (
+//                 <ListItem
+//                   key={session.id}
+//                   style={{
+//                     display: "inline-block",
+//                     width: "auto",
+//                     marginRight: "20px",
+//                   }}
+//                 >
+//                   <Card
+//                     elevation={6}
+//                     className="customCard"
+//                     sx={{
+//                       minWidth: 275,
+//                       maxWidth: 345,
+//                       borderRadius: "20px",
+//                       boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.5)",
 //                     }}
-//                   />
-//                   <Typography
-//                     variant="h5"
-//                     component="h2"
-//                     style={{ fontWeight: "bold" }}
 //                   >
-//                     {session.name}
-//                   </Typography>
-//                   <Typography variant="body1">
-//                     Location: {session.address}
-//                   </Typography>
-//                   <Typography variant="body1">
-//                     Date: {session.date}
-//                   </Typography>
-//                   <Typography variant="body1">
-//                     Slots: {session.slots} slot(s) left
-//                   </Typography>
-//                 </CardContent>
-//               </Card>
-//             </ListItem>
-//           ))}
-//         </List>
+//                     <CardContent>
+//                       <img
+//                         src={UluPandan}
+//                         alt="Ulu Pandan"
+//                         style={{
+//                           width: "100%",
+//                           height: "auto",
+//                           marginBottom: "10px",
+//                           borderRadius: "8%",
+//                         }}
+//                       />
+//                       <Typography
+//                         variant="h5"
+//                         component="h2"
+//                         style={{ fontWeight: "bold" }}
+//                       >
+//                         {session.name}
+//                       </Typography>
+//                       <Typography variant="body1">
+//                         Location: {session.address}
+//                       </Typography>
+//                       <Typography variant="body1">
+//                         Date: {session.date}
+//                       </Typography>
+//                       <Typography variant="body1">
+//                         Slots: {session.slots} slot(s) left
+//                       </Typography>
+//                     </CardContent>
+//                   </Card>
+//                 </ListItem>
+//               ))}
+//             </List>
+//           </Box>
+//         </div>
 //       ) : (
-//         <p>No sessions found.</p>
+//         <div>
+//           <p>
+//             Showing{" "}
+//             <span style={{ fontSize: "120px", fontWeight: "bold" }}>
+//               Joined
+//             </span>{" "}
+//             Sessions...
+//           </p>
+//         </div>
 //       )}
-//     </>
-//   );
-
-//   const handleCardClick = (sessionId) => {
-//     navigate(`/SessionDetails/${sessionId}`);
-//   };
-
-//   return (
-//     <div>
-//       <Navbar />
-//       {/* Container for both sections */}
-//       <div style={{ display: 'flex', flexDirection: 'column', height: 'calc(100vh - 70px)' }}> {/* Adjusted for navbar height */}
-        
-//         {/* Hosted Sessions */}
-//         <div style={{ flex: 1, display: 'flex', flexDirection: 'column', padding: '20px' }}>
-//           <h2 style={{ margin: 0 }}>Hosted Sessions</h2>
-//           {renderSessionsList(hostedSessions)}
-//         </div>
-        
-//         {/* Joined Sessions */}
-//         <div style={{ flex: 1, display: 'flex', flexDirection: 'column', padding: '20px' }}>
-//           <h2 style={{ margin: 0 }}>Joined Sessions</h2>
-//           {renderSessionsList(joinedSessions)}
-//         </div>
-        
-//       </div>
-//     </div>
-//   );
-  
-
-        
-// };
-
-// export default MySessions;
-
-
-  
-
-
-
-
-// import React from 'react';
-// import Navbar from '../components/Navbar';
-
-// const MySessions = () => {
-//   return (
-//     <div>
-//       <Navbar />
-//       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '40px', marginBottom: '30px' }}>
-//         <p>hello</p>
-//         {/* Display hosted workout session here. */}
-//       </div>
 //     </div>
 //   );
 // };
